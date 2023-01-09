fbdbb399a2781b6e1a3e243a7aaff48eb623a3fe
Brian <wamwangimathenge@gmail.com>
2023-01-09 13:49:47 +0300

    Vanilla JS Version Commented In

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Vanilla JavaScript Version
+// const header = document.createElement("h1")
+// header.id = "header"
+// header.classname = "blue"
+// header.textContent = "Hello from Vanilla JS!"
+// document.getElementById("root").append(header)
+
+
+// JSX (JavaScript XML)
+
+const element = <h1 id="header" className="blue">Hello!</h1>
+
 ReactDOM.render(
     // What we want to display (our entire app)
-    <h1>Hello!</h1>,
+    element,
     // Where we want to display it
     document.getElementById("root")
 )
