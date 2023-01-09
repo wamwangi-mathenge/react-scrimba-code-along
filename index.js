1c60d7fce7060e790cb673b094340e1dc378e7af
Brian <wamwangimathenge@gmail.com>
2023-01-09 13:41:32 +0300

    ReactDOM.render added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 ReactDOM.render(
     // What we want to display (our entire app)
-
+    <h1>Hello!</h1>,
     // Where we want to display it
-    
+    document.getElementById("root")
 )
