23e327317588d313a89406f7cec29389132599c5
Brian <wamwangimathenge@gmail.com>
2023-01-09 13:31:43 +0300

    React & ReactDOM imported

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello!")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    // What we want to display (our entire app)
+
+    // Where we want to display it
+    
+)
